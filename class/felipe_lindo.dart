7041ff7e4792c6232c363515ce8aea8bc3570bf3
Felipe <x318737@prservicos.com.br>
2024-04-03 11:54:34 -0300

    Subindo tudo

---
+++ class/felipe_lindo.dart
@@ -0,0 +1,7 @@
+class Person {
+  String name;
+  int age;
+  String gender;
+
+  Person(this.name, this.age, this.gender);
+}
---
+++ class/mirella_linda.dart
@@ -0,0 +1,11 @@
+class MirellaLinda {
+  final String title;
+  final String subtitle;
+  final String boyFriend;
+
+  const MirellaLinda({
+    this.title = 'A mais linda',
+    this.subtitle = 'e goxtosa',
+    this.boyFriend = 'Felipe',
+  });
+}
